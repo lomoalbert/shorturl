c2a482a9efdfccc303514b7db08c4ff2d5a5d72c
albert <albert@spiritframe.com>
2016-11-19 17:37:54 +0800

    add:  QR

--- controllers/ShortURL.go
+++ controllers/ShortURL.go
@@ -2,9 +2,10 @@ package controllers
 
 import (
 	"github.com/lomoalbert/shorturl/models"
-
+	qrcode "github.com/skip2/go-qrcode"
 	"github.com/astaxie/beego"
 	"net/url"
+	"strconv"
 )
 
 // Operations about object
@@ -54,6 +55,21 @@ func (ctl *ShortURLController) Get() {
 	beego.Debug(surl)
 	ctl.Data["json"]=surl
 	ctl.ServeJSON()
+}
+
+// @Title Get QrCode Image
+// @Description get QRcode Image
+// @Param	code		path 	string	true		"The short code"
+// @Success 200 {object} models.SURL
+// @Failure 403 body is empty
+// @router /:code/qr [get]
+func (ctl *ShortURLController) Qr() {
+	code := ctl.GetString(":code")
+	data,err := qrcode.Encode(ctl.Ctx.Input.Scheme()+"://"+ctl.Ctx.Input.Host()+":"+strconv.Itoa(ctl.Ctx.Input.Port())+"/"+code,qrcode.Medium,256)
+	if err!=nil{
+		ctl.Abort("500")
+	}
+	ctl.Ctx.ResponseWriter.Write(data)
 }
 
 func (ctl *RedirectController) Get(){
